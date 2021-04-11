387586d76539a8b00926479a6c930ddc8ad0508d
Santiago Moyano <santiago98moy@gmail.com>
2021-04-11 00:28:03 -0300

    finalizado el ejercicio 3 de subprocess popen

---
+++ tp2subprocess_popen/ej3popen.py
@@ -0,0 +1,48 @@
+import subprocess as sp 
+import sys
+import getopt
+import datetime as dt
+
+opt,arg = getopt.getopt(sys.argv[:1], 'c:f:l:')
+
+if len(opt) != 3:
+    print("enter the arguments correctly")
+    exit()
+
+log_file = ''
+command = ''
+output_file = ''
+
+for (op,ar) in opt:
+    if op = '-c':
+        command = ar
+    elif op == '-f':
+        output_file = open(ar, "a")
+    elif op == '-l':
+        log_file = ar
+
+
+process = sp.Popen([command], stdout=output_file, stderr=sp.PIPE, shell=True, universal_newlines=True)
+error = process.communicate()[1]
+
+if not error:
+    date = dt.datetime.now()
+    for_write = f"{date}: the command: {command}, it ran correctly"
+    file_write = open(log_file, "a")
+    file_write.write(to_write)
+    file_write.write("\n")
+    file_write.close() 
+    
+else:
+    date = dt.datetime.now()
+    for_write = f"{date}: >> {err}"
+    file_write = open(log_file, "a")
+    file_write.write(for_write)
+    file_write.write("\n")
+    file_write.close()
+    
+output_file.writelines("\n")
+output_file.close()
+
+    
+
